2e1e8880007de3ea3d55ea513a2fd1bb36519613
Sergey Gerasimov <svlgerasimov@yandex.ru>
2022-09-27 10:23:11 +0300

    feat: добавть в ExceptionHandler обработку произвольного исключения, добавть запись stack trace в лог

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,7 +21,7 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNotFoundException(NotFoundException exception) {
         Map<String, String> result = Map.of("Not Found Error", exception.getMessage());
-        log.warn(String.valueOf(result));
+        log.warn(String.valueOf(result), exception);
         return result;
     }
 
@@ -33,7 +34,25 @@ public class ErrorHandler {
                                 String.format("Validation Error in field '%s' with value = '%s'",
                                         fieldError.getField(), fieldError.getRejectedValue()),
                         fieldError -> Objects.requireNonNullElse(fieldError.getDefaultMessage(), "")));
-        log.warn(String.valueOf(result));
+        log.warn(String.valueOf(result), exception);
+        return result;
+    }
+
+    // HttpMessageNotReadableException выбрасывается например, если отсутствует тело запроса
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        String message = exception.getMessage();
+        Map<String, String> result = Map.of("Bad Request", Objects.isNull(message) ? "Details unknown" : message);
+        log.warn(String.valueOf(result), exception);
+        return result;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleUnexpectedException(Throwable exception) {
+        Map<String, String> result = Map.of("Internal Server Error", exception.getMessage());
+        log.warn(String.valueOf(result), exception);
         return result;
     }
 }
