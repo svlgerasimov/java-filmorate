eac764b8e061d6c669859c722f17dccbfd04d0c0
svlgerasimov <svlgerasimov@yandex.ru>
2022-11-12 00:06:54 +0300

    fix: import в InMemoryFIlmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryFilmStorage.java
@@ -6,14 +6,7 @@ import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.LikesStorage;
 import ru.yandex.practicum.filmorate.util.IdGenerator;
 
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
